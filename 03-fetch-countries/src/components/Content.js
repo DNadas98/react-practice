614ca083f87818193acb0fd67d859d4f6f1d801a
DNadas98 <125133206+DNadas98@users.noreply.github.com>
2023-06-02 23:11:51 +0200

    Refactor

--- 03-fetch-countries/src/components/Content.js
+++ 03-fetch-countries/src/components/Content.js
@@ -7,65 +7,54 @@ function Content() {
   const [isLoading, setLoading] = useState(true);
   const [countriesArray, setCountriesArray] = useState([]);
   const [favouriteCountryNames, setFavouriteCountryNames] = useState([]);
-
   const [searchInput, setSearchInput] = useState("");
   const [showFavourites, setShowFavourites] = useState(false);
   const [sortButtonState, setSortButtonState] = useState(true);
   const [countriesToShow, setCountriesToShow] = useState([]);
 
-  // Load data, local storage
+  //Fetch data from API
   useEffect(() => {
     LoadData(setCountriesArray, setLoading);
-    console.log(`loadData\t${countriesArray.length}`);
   }, []);
-
-  //Load favourites from local storage, add to local storage
+  //Load favourites from local storage
   useEffect(() => {
     try {
       const localFavouritesArray = JSON.parse(localStorage.getItem("favourites")) || [];
       if (Array.isArray(localFavouritesArray) && localFavouritesArray.length >= 0) {
         setFavouriteCountryNames(localFavouritesArray);
-        console.log(`setFavouriteCountryNames\t${localFavouritesArray}`);
       }
     } catch (err) {
       console.error(err);
     }
   }, []);
-
+  //Update the local storage
   useEffect(() => {
     if (favouriteCountryNames.length >= 1) {
       localStorage.setItem("favourites", JSON.stringify(favouriteCountryNames));
-      console.log(`localStorage.setItem\t${favouriteCountryNames}`);
     }
   }, [favouriteCountryNames]);
-
+  //Set countriesToShow
+  useEffect(() => {
+    setCountriesToShow(countriesArray.sort((a, b) => a.name.common.localeCompare(b.name.common)));
+  }, [countriesArray]);
   //Apply filters
   useEffect(() => {
-    console.log("asd");
     let filteredCountries = [...countriesArray];
-
     if (searchInput !== "") {
       filteredCountries = filteredCountries.filter((country) =>
         country.name.common.toLowerCase().includes(searchInput.toLowerCase())
       );
     }
-
     if (showFavourites) {
       filteredCountries = filteredCountries.filter((country) => favouriteCountryNames.includes(country.name.common));
     }
-
     if (sortButtonState) {
       filteredCountries = filteredCountries.sort((a, b) => a.name.common.localeCompare(b.name.common));
     } else {
       filteredCountries = filteredCountries.sort((a, b) => b.name.common.localeCompare(a.name.common));
     }
-
     setCountriesToShow(filteredCountries);
   }, [searchInput, showFavourites, sortButtonState, favouriteCountryNames]);
-
-  useEffect(() => {
-    setCountriesToShow(countriesArray.sort((a, b) => a.name.common.localeCompare(b.name.common)));
-  }, [countriesArray]);
 
   return (
     <div className="Content">
